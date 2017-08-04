cf3140cb415ab44c6c93566fc9710a4bddb8c3fb
cyang2 <christopheryang2019@u.northwestern.edu>
2017-08-04 12:12:55 -0500

    get quotes to fade in and out at the bottom of the page

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,18 @@
+function showNextQuote(index) {
+  const allQuotes = $('.quote');
+
+  const quote = allQuotes.eq(index % allQuotes.length);
+  const wordCount = quote.html().split(" ").length;
+  const delay = wordCount * 100;
+
+  quote.delay(3000).fadeIn(2000).delay(delay).fadeOut(2000);
+
+  setTimeout(function () {
+    showNextQuote(++index);
+  }, 4000 + delay);
+};
+
 $(document).ready(function () {
-  $(".header").fadeIn(3000);
+  $('.header').fadeIn(3000);
+  showNextQuote(0);
 });

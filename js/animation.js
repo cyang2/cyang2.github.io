6ae8bf9609884e27fb935df8e7eb5affcae4ec55
cyang2 <christopheryang2019@u.northwestern.edu>
2017-08-05 15:56:40 -0500

    creation of basic animation (square-to-circle)

--- js/animation.js
+++ js/animation.js
@@ -3,9 +3,9 @@ function showNextQuote(index) {
 
   const quote = allQuotes.eq(index % allQuotes.length);
   const wordCount = quote.html().split(" ").length;
-  const delay = wordCount * 100;
+  const delay = wordCount * 140;
 
-  quote.delay(3000).fadeIn(2000).delay(delay).fadeOut(2000);
+  quote.delay(3000).fadeIn(1500).delay(delay).fadeOut(2000);
 
   setTimeout(function () {
     showNextQuote(++index);
@@ -14,5 +14,6 @@ function showNextQuote(index) {
 
 $(document).ready(function () {
   $('.header').fadeIn(3000);
-  showNextQuote(0);
+  const randomNumber = Math.floor(Math.random() * 20);
+  showNextQuote(randomNumber);
 });

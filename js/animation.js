305313b0917d145f40d19d2364ffde72c93d76c1
cyang2 <christopheryang2019@u.northwestern.edu>
2017-08-10 10:02:29 -0500

    add avatars and toggling functionality to the page

--- js/animation.js
+++ js/animation.js
@@ -7,15 +7,64 @@ function showNextQuote(index) {
 
   quote.delay(3000).fadeIn(1500).delay(delay).fadeOut(2000);
 
-  setTimeout(function () {
-    showNextQuote(++index);
-  }, 4000 + delay);
+  setTimeout( function () { showNextQuote(++index); }, 4000 + delay);
+};
+
+function toggleAvatars() {
+  $('.resume').hover(
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smile-img.png");
+    },
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smirk-img.png");
+    }
+  );
+
+  $('.code').hover(
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-tongue-img.png");
+    },
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smirk-img.png");
+    }
+  );
+
+  $('.company').hover(
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-laugh-img.png");
+    },
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smirk-img.png");
+    }
+  );
+
+  $('.film').hover(
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-surprised-img.png");
+    },
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smirk-img.png");
+    }
+  );
+
+  $('.contact').hover(
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smile-img.png");
+    },
+    function () {
+      $('.avatar').attr("src", "img/avatar/avatar-smirk-img.png");
+    }
+  );
 };
 
 $(document).ready(function () {
   $('.header').animate({ opacity: 1 }, 5000);
-  $('.cool-thing').delay(2000).animate({ opacity: 1 }, 5000);
+  $('.avatar-wrapper').animate({ opacity: 1 }, 5000);
   $('.svg-wrapper').animate({ opacity: 1 }, 5000);
-  const randomNumber = Math.floor(Math.random() * 20);
-  showNextQuote(randomNumber);
+
+  // $('.cool-thing').delay(2000).animate({ opacity: 1 }, 5000);
+  toggleAvatars();
+
+  const randomStart = Math.floor(Math.random() * 20);
+  showNextQuote(randomStart);
 });

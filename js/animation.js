edcd4df624d10191200701bd78e9bf88cac11a24
cyang2 <christopheryang2019@u.northwestern.edu>
2017-08-27 23:35:27 -0500

    use ES6 arrow notation for functions

--- js/animation.js
+++ js/animation.js
@@ -7,66 +7,46 @@ function showNextQuote(index) {
 
   quote.fadeIn(1500).delay(delay).fadeOut(2000);
 
-  setTimeout( function () { showNextQuote(++index); }, 3500 + delay);
+  setTimeout(() => { showNextQuote(++index); }, 3500 + delay);
 };
 
 function toggleAvatars() {
   $('.resume').hover(
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smile.png");
-    },
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smirk.png");
-    }
+    () => { $('.face').attr("src", "img/avatar/avatar-smile.png") },
+    () => { $('.face').attr("src", "img/avatar/avatar-smirk.png") }
   );
 
   $('.code').hover(
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-tongue.png");
-    },
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smirk.png");
-    }
+    () => { $('.face').attr("src", "img/avatar/avatar-tongue.png") },
+    () => { $('.face').attr("src", "img/avatar/avatar-smirk.png") }
   );
 
   $('.company').hover(
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-laugh.png");
-    },
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smirk.png");
-    }
+    () => { $('.face').attr("src", "img/avatar/avatar-laugh.png") },
+    () => { $('.face').attr("src", "img/avatar/avatar-smirk.png") }
   );
 
   $('.film').hover(
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-surprised.png");
-    },
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smirk.png");
-    }
+    () => { $('.face').attr("src", "img/avatar/avatar-surprised.png") },
+    () => { $('.face').attr("src", "img/avatar/avatar-smirk.png") }
   );
 
   $('.contact').hover(
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-eyebrow.png");
-    },
-    function () {
-      $('.face').attr("src", "img/avatar/avatar-smirk.png");
-    }
+    () => { $('.face').attr("src", "img/avatar/avatar-eyebrow.png") },
+    () => { $('.face').attr("src", "img/avatar/avatar-smirk.png") }
   );
 };
 
 function animateHeaderLinks() {
-  $('.header-link').on('mouseenter', function() {
+  $('.header-link').on('mouseenter', function () {
     $(this).velocity({ translateY: -3.5 }, { delay: 0, duration: 200 });
   });
-  $('.header-link').on('mouseleave', function() {
+  $('.header-link').on('mouseleave', function () {
     $(this).velocity('stop').velocity('reverse');
   });
 };
 
-$(document).ready(function () {
+$(document).ready(() => {
   $('.header').velocity({ opacity: 1 }, 5000);
   $('.cover').velocity({ opacity: 1 }, 5000);
   $('.avatar').velocity({ opacity: 1 }, 5000);
